a1bfc9a2eb4a9ba371c964adf4ad981c990dfb55
sa5m.chavez <sa5m.escom@gmail.com>
2024-11-16 23:09:52 -0600

    feat: repositories and docker usage

---
+++ deploy/local/base.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS feeds(
+    id VARCHAR(14) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+)
